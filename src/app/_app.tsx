b67ed7df42127ee1146d10ca62afcb209c424edd
Niranjan <niranjanr753@gmail.com>
2024-10-05 01:40:26 +0530

    _app.tsx file

---
+++ src/app/_app.tsx
@@ -0,0 +1,9 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
